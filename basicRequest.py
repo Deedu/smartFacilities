2f7d2e96fe2b56157f499e92b8568f83639561e2
David Daugela <daviddaugela@gmail.com>
2017-11-19 09:46:55 -0500

    Update basicRequest.py

--- basicRequest.py
+++ basicRequest.py
@@ -11,6 +11,7 @@ peopleSinceLastClean = 0
 methaneLevel=0
 buttonPushedCount = 0  # Button is pushed to reset count
 doorPassConstant =100
+previousPushTrueCount = 0
 
 while True:
     line = ser.readline()
@@ -25,10 +26,16 @@ while True:
         peopleSinceLastClean +=1
     if buttonPushed=="1":
         buttonPushedCount +=1
+        previousPushTrueCount +=1
+    else:
+        previousPushTrueCount=0
     photoLevel = int(line[2])
     methaneLevel1 = int(line[3])
     methaneLevel2 = int(line[4])
 
+    if(previousPushTrueCount >5):
+        peopleSinceLastClean = 0
+        
     names = ["peopleSinceLastClean","photoLevel", "methaneLevel1","methaneLevel2", "buttonPushCount"]
     datasets = [peopleSinceLastClean,photoLevel,methaneLevel1,methaneLevel2, buttonPushedCount]
 
@@ -38,5 +45,7 @@ while True:
 
     r = requests.post("http://www.dweet.io/dweet/for/lesDragooner1",
                       data=dataDict)
+    if(peopleSinceLastClean >=50):
+        r2 = requests.post("https://hooks.zapier.com/hooks/catch/2688900/s9rhbo/", data={'doge': "Orange doge"})
 
     print(line)
