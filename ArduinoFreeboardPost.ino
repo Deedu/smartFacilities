68c120a756bfc6524d5f3d2234ce0e4837949b70
David Daugela <daviddaugela@gmail.com>
2017-11-19 04:52:12 -0500

    Update ArduinoFreeboardPost.ino

--- ArduinoFreeboardPost.ino
+++ ArduinoFreeboardPost.ino
@@ -1,32 +1,55 @@
- Blink
- Turns on an LED on for one second, then off for one second, repeatedly.
- 
- This example code is in the public domain.
- */
+//Pin Layout Constants
+const unsigned int led = 13;
+const unsigned int methaneInp1=A1;
+const unsigned int methaneInp2 = A2;
+const unsigned int photoInp = A0;
+const unsigned int sonicTrigInp = 11;
+const unsigned int sonicEchoInp = 12;
+const unsigned int buttonInp = 4;
+String writeString = "";
 
-// Pin 13 has an LED connected on most Arduino boards.
-// give it a name:
-int led = 13;
 
 // the setup routine runs once when you press reset:
 void setup() {         
 Serial.begin(9600);
   // initialize the digital pin as an output.
+  pinMode(sonicTrigInp,OUTPUT);
+  pinMode(sonicEchoInp,INPUT);
   pinMode(led, OUTPUT);     
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  Serial.println("tre");
-  Serial.println(analogRead(0));
-  Serial.println("1");
-  Serial.println(analogRead(A1));
-  Serial.println("2");
-  Serial.println(analogRead(A2));
-  //gitalWrite(led, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(200);            // wait for a second
-  digitalWrite(led, LOW);    // turn the LED off by making the voltage LOW
-  delay(200); 
-  Serial.println(digitalRead(7));
 
+  //Read First sensor, button
+  int buttonPush = digitalRead(buttonInp);
+  String buttonPushVal = String(buttonPush);
+  writeString += buttonPushVal + "~";
+
+  //Do a distance check
+  digitalWrite(sonicTrigInp,LOW);
+  delayMicroseconds(2);
+  digitalWrite(sonicTrigInp,HIGH);
+  delayMicroseconds(2);
+  digitalWrite(sonicTrigInp,LOW);
+  long duration = pulseIn (sonicEchoInp, HIGH);
+  int distance = duration/29/2;
+  String distVal = String(distance);
+  writeString += distVal + "~";
+
+  //PhotoSensor
+  int photo = analogRead(0);
+  String photoVal = String(photo);
+  writeString += photoVal + "~";
+
+  //Methane
+  int methane1 = analogRead(methaneInp1);
+  String methane1Val = String(methane1);
+  writeString += methane1Val + "~";
+  int methane2= analogRead(methaneInp2);
+  String methane2Val = String(methane2);
+  writeString += methane2Val;
+
+  Serial.print(writeString);
+  
 }
