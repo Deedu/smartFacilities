07369d8d0d274006cd060a5fa81faf2d82efab04
David Daugela <daviddaugela@gmail.com>
2017-11-19 06:58:10 +0000

    Add files via upload

--- basicRequest.py
+++ basicRequest.py
@@ -1,4 +1,17 @@
 import requests
+import serial
 
 r=requests.post("http://www.dweet.io/dweet/for/nixon-is-pres1",
                 data={"DogeProperty": 'Magnificent'})
+
+serial_port = '/dev/ttyACM0'
+baud_rate = 9600 #In arduino, Serial.begin(baud_rate)
+write_to_file_path = "output.txt"
+
+output_file = open(write_to_file_path, "w+")
+ser = serial.Serial(serial_port, baud_rate)
+while True:
+    line = ser.readline()
+    line = line.decode("utf-8") #ser.readline returns a binary, convert to string
+    print(line)
+    output_file.write(line)
